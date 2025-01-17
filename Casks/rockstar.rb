001543c2368ef87e8e4605e80cd3c3a6487b9666
Himanshu Pandey <himanshu.dn.pandey@gmail.com>
2025-01-18 01:52:20 +0530

    Add cask for rockstar

---
+++ Casks/rockstar.rb
@@ -0,0 +1,20 @@
+cask "rockstar" do
+  version "1.4"
+  sha256 "1b80c0b940f9f6a6f4cf416f6f438298ad88c99db70b704c25c7c548e01bbf73"
+
+  url "https://github.com/hp77-creator/rockstar/releases/download/v#{version}/Rockstar.dmg"
+  name "Rockstar"
+  desc "A powerful clipboard manager for macOS with Obsidian sync capabilities"
+  homepage "https://github.com/hp77-creator/rockstar"
+
+  depends_on macos: ">= :ventura" # Based on LSMinimumSystemVersion in Info.plist (13.0)
+
+  app "Rockstar.app"
+
+  zap trash: [
+    "~/Library/Application Support/Rockstar",
+    "~/Library/Preferences/com.hp77.ClipboardManager.plist",
+    "~/Library/Caches/Rockstar",
+    "~/Library/Logs/Rockstar"
+  ]
+end
